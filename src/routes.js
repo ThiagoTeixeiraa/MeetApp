8659dfacf7cf8612dfaf1511b8b9906767e60736
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-08-30 20:02:00 -0300

    adicionado gerenciamento de arquivos no projeto

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/Multer';
 
 import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/Auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
-routes.put('/users', authMiddleware, UserController.update);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', authMiddleware, UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
