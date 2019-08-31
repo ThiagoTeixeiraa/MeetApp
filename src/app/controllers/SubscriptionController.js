da180bc663060fbc4214e27fba6bb40a9b9890a0
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-08-31 14:52:38 -0300

    criado endpoint para inscrição me meetup

---
+++ src/app/controllers/SubscriptionController.js
@@ -0,0 +1,65 @@
+import Subscription from '../models/Subscription';
+import Meetup from '../models/Meetup';
+
+class SubscriptionController {
+  async store(req, res) {
+    const meetup = await Meetup.findOne({
+      where: {
+        id: req.params.meetupId,
+      },
+    });
+
+    if (meetup.user_id === req.userId) {
+      res.status(400).json({ error: 'Organizer cannot subscript for meetup' });
+    }
+
+    if (meetup.past) {
+      return res.status(400).json({
+        error: "you can't subscript for meetups that already happened",
+      });
+    }
+
+    const isSubscribed = await Subscription.findOne({
+      where: {
+        user_id: req.userId,
+        meetup_id: meetup.id,
+      },
+    });
+
+    if (isSubscribed) {
+      return res.status(400).json({
+        error: 'You already subscribed to this meetup',
+      });
+    }
+
+    const checkDate = await Subscription.findOne({
+      where: {
+        user_id: req.userId,
+      },
+      include: [
+        {
+          model: Meetup,
+          required: true,
+          where: {
+            date: meetup.date,
+          },
+        },
+      ],
+    });
+
+    if (checkDate) {
+      return res
+        .status(400)
+        .json({ error: "Can't subscribe to two meetups at the same time" });
+    }
+
+    const subscription = await Subscription.create({
+      user_id: req.userId,
+      meetup_id: meetup.id,
+    });
+
+    return res.json(subscription);
+  }
+}
+
+export default new SubscriptionController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
 import OrganizerController from './app/controllers/OrganizerController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 
 import authMiddleware from './app/middlewares/Auth';
 
@@ -26,6 +27,8 @@ routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
 
 routes.get('/organizer', OrganizerController.index);
+
+routes.post('/meetups/:meetupId/subscriptions', SubscriptionController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
